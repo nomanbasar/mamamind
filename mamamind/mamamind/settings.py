000a731b3e446d8be507cd1bd69c0e90ce8bad42
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-18 12:36:04 +0600

    email configuration

--- mamamind/mamamind/settings.py
+++ mamamind/mamamind/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
