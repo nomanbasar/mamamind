6f5275d03009dc9eb905bcfbeb66ca52ed22d775
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-18 15:36:02 +0600

    urls for faq

---
+++ mamamind/faqs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import FAQListCreateView, FAQDetailView
+
+
+urlpatterns = [
+    path("", FAQListCreateView.as_view()),
+    path("<int:id>/", FAQDetailView.as_view()),
+]
