859545f997947b0658fcdf33114e6c770e210bdb
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-18 16:25:18 +0600

    admin configure for contact

--- mamamind/contact/admin.py
+++ mamamind/contact/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactMessage
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "enquiry_type",
+        "subject",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "enquiry_type",
+        "status",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "email",
+        "subject",
+        "message",
+    )
+    ordering = ("-id",)
