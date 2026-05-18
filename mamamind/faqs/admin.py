4187102a63b93e54fc1f4797951e125b5d33c5e6
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-18 15:38:56 +0600

    admin configuration for faq

--- mamamind/faqs/admin.py
+++ mamamind/faqs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FAQ
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "question",
+        "category",
+        "is_active",
+        "created_at",
+    )
+    list_filter = ("category", "is_active")
+    search_fields = ("question", "answer")
+    ordering = ("-id",)
