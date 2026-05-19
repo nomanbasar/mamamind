645a26fcb3ceac8cf4c9d2b10384221919fcbff4
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-19 14:56:51 +0600

    cors install

--- mamamind/mamamind/settings.py
+++ mamamind/mamamind/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,7 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 AUTH_USER_MODEL = 'authentication.User'
