355b4f6375c3cf0551a1f541e1eb713ec4a2905e
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-05-22 15:54:41 +0600

    secret key configure

--- mamamind/mamamind/settings.py
+++ mamamind/mamamind/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
@@ -22,8 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w9f)g^t$&k%ugsrve22#@lj=(8x@gl$k-7wl0l64xt%q1xz!2+'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
