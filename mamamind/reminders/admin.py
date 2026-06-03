dfdd5ccac1d0da04534944fab833f0b43c5023f5
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-06-03 12:46:06 +0600

    admin configure for reminder

--- mamamind/reminders/admin.py
+++ mamamind/reminders/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reminder
+
+
+@admin.register(Reminder)
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "family",
+        "owner",
+        "created_by",
+        "visibility",
+        "recurring",
+        "reminder_date",
+        "reminder_time",
+        "is_completed",
+        "created_at",
+    )
+    list_filter = (
+        "visibility",
+        "recurring",
+        "is_completed",
+        "reminder_date",
+        "family",
+    )
+    search_fields = (
+        "title",
+        "family__name",
+        "owner__email",
+        "owner__full_name",
+        "created_by__email",
+        "created_by__full_name",
+    )
