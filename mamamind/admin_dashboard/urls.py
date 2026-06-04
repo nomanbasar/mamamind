27d9beb097188a912b476db5fc91c1f8c4fc7986
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-06-04 15:45:06 +0600

    url configure for admin dashboard

--- mamamind/admin_dashboard/urls.py
+++ mamamind/admin_dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import AdminDashboardOverviewView
+from .views import AdminDashboardOverviewView, AdminUserListView
 
 urlpatterns = [
     path("overview/", AdminDashboardOverviewView.as_view()),
+    path("users/", AdminUserListView.as_view()),
 ]
