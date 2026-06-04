cb44ee54afe1c1f7647fd7cc4525a8282dc9e791
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-06-04 16:13:33 +0600

    urls for AdminSubscriptionListView

--- mamamind/admin_dashboard/urls.py
+++ mamamind/admin_dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AdminDashboardOverviewView, AdminUserListView
+from .views import AdminDashboardOverviewView, AdminUserListView, AdminSubscriptionListView
 
 urlpatterns = [
     path("dashboard/overview/", AdminDashboardOverviewView.as_view()),
     path("users/", AdminUserListView.as_view()),
+    path("subscriptions/", AdminSubscriptionListView.as_view()),
 ]
